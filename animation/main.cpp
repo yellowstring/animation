d45485b59229f2689202cfbcb715087f4fb0a296
Cole Zich <zich.cole@gmail.com>
2020-04-25 12:50:03 -0400

    Initial commit

---
+++ animation/main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  animation
+//
+//  Created by Cole Zich on 4/25/20.
+//  Copyright © 2020 Cole Zich. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "guy falls on the ground, flipbook (2020)\n";
+    std::cout << "Press anything to progress\n";
+    std::cin.get();
+    
+    printf("\033\n");
+    
+    std::cout << "_____\n";
+    std::cout << "(o o)\n";
+    std::cout << "( o )\n";
+    std::cout << "  |  \n";
+    std::cout << " / | \n";
+    std::cout << "Press anything to progress\n";
+    std::cin.get();
+    
+    printf( "\033\n" );
+    
+    std::cout << "   ___\n";
+    std::cout << "  (o o)\n";
+    std::cout << "   ( o )\n";
+    std::cout << "     : \n";
+    std::cout << "      : \n";
+    std::cout << "     / | \n";
+    std::cout << "Press anything to progress\n";
+    std::cin.get();
+    
+    printf( "\033\n" );
+    
+    std::cout << "    _\n";
+    std::cout << "    o)\n";
+    std::cout << "  (o\n";
+    std::cout << "    | \n";
+    std::cout << "   / | \n";
+    std::cout << "Press anything to progress\n";
+    std::cin.get();
+    
+    printf( "\033\n" );
+    
+    std::cout << "\n";
+    std::cout << "\n";
+    std::cout << "(o o)\n";
+    std::cout << "( O )\n";
+    std::cout << "  |\n";
+    std::cout << " _|_\n";
+    std::cout << "That's it!\n";
+    
+    return 0;
+}
